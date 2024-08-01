73aa878a35052eacd3bdf677e66c2d153a93e415
hc8756 <hc8756@rit.edu>
2024-08-01 13:09:02 -0400

    day2

--- gamestarter/Assets/Scripts/Player.cs
+++ gamestarter/Assets/Scripts/Player.cs
@@ -6,11 +6,13 @@ public class Player : MonoBehaviour
 {
     private Rigidbody2D body;
     [SerializeField] private float speed;
+    private bool grounded;
     // Start is called before the first frame update
     void Start()
     {
         body = GetComponent<Rigidbody2D>();
         speed = 10;
+        grounded = false;
     }
 
     // Update is called once per frame
@@ -19,6 +21,21 @@ public class Player : MonoBehaviour
         body.velocity = new Vector2(Input.GetAxis("Horizontal")*speed,body.velocity.y);
         if (Input.GetKeyDown(KeyCode.Space)) {
             body.velocity = new Vector2(body.velocity.x, speed);
+            grounded = false;
+        }
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        else if (Input.GetAxis("Horizontal") < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.gameObject.tag == "Ground") {
+            grounded = true;
         }
     }
 }
